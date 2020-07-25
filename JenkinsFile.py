4b205b158821b535f2e3f6eac2e38e7fb7f45eb6
Lokesh Khandelwal <42270097+lokeshkh92@users.noreply.github.com>
2020-07-25 21:11:53 +0530

    Update JenkinsFile.py

--- JenkinsFile.py
+++ JenkinsFile.py
@@ -1,11 +1,2 @@
 print "Hello Lokesh!!!"
-pipeline {
-    agent { docker { image 'python:3.5.1' } }
-    stages {
-        stage('build') {
-            steps {
-                sh 'python --version'
-            }
-        }
-    }
-}
+print "Welcome TO Jenkins!!!"
